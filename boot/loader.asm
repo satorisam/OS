750ac5ddbf51077f59bf468f63b15e7eb73d69e9
yaut <3340618946@qq.com>
2022-07-30 22:19:45 +0800

    4.3

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,13 +1,67 @@
 %include "boot.inc"
 
 SECTION loader vstart=LOADER_BASE_ADDR
-    mov byte [gs:0x00],'2'
-    mov byte [gs:0x02],' '
-    mov byte [gs:0x04],'L'
-    mov byte [gs:0x06],'O'
-    mov byte [gs:0x08],'A'
-    mov byte [gs:0x0a],'D'
-    mov byte [gs:0x0c],'E'
-    mov byte [gs:0x0e],'R'
+LOADER_STACK_TOP equ LOADER_BASE_ADDR
+jmp loader_start
 
+    GDT_BASE: dd 0x00000000
+              dd 0x00000000
+    CODE_DESC: dd 0x0000FFFF
+               dd DESC_CODE_HIGH4
+    DATA_STACK_DESC: dd 0x0000FFFF
+                     dd DESC_DATA_HIGH4
+    VIDEO_DESC: dd 0x80000007
+                dd DESC_VIDEO_HIGH4
+    
+    GDT_SIZE equ $ - GDT_BASE
+    GDT_LIMIT equ GDT_SIZE - 1
+    times 60 dq 0
+    
+    SELECTOR_CODE equ (0x0001<<3) + TI_GDT + RPL0
+    SELECTOR_DATA equ (0x0002<<3) + TI_GDT + RPL0
+    SELECTOR_VIDEO equ (0x0003<<3) + TI_GDT + RPL0
+
+    gdt_ptr dw GDT_LIMIT
+            dd GDT_BASE
+    loadermsg db '2 loader in real. I'm your father.'
+
+    loader_start:
+    
+    mov sp,LOADER_BASE_ADDR
+    mov bp,loadermsg
+    mov cx,34
+    mov ax,0x1301
+    mov bx,0x001f
+    mov dx,0x1800
+    int 0x10
+
+    in al,0x92
+    or al,00000010b
+    out 0x92,al
+
+    lgdt [gdt_ptr]
+
+    mov eax,cr0
+    or eax,0x00000001
+    mov cr0,eax
+
+    jmp dword SELECTOR_CODE:p_mode_start
+
+
+[bits 32]
+    p_mode_start:
+    mov ax,SELECTOR_DATA
+    mov ds,ax
+    mov es,ax
+    mov ss,ax
+    mov esp,LOADER_STACK_TOP
+    mov ax,SELECTOR_VIDEO
+    mov gs,ax
+
+    mov byte [gs:160], 'P'
+
+
+
+
+    
     jmp $
