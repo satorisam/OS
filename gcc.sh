343f5a576b38b95e11b63931dca7b3500b1dfb18
yaut <3340618946@qq.com>
2022-08-01 00:07:43 +0800

    新增put_char()

--- gcc.sh
+++ gcc.sh
@@ -1,4 +1,5 @@
-gcc -Wall -m32 -fno-stack-protector -c -o build/main.o kernel/main.c
-ld -m elf_i386 build/main.o -Ttext 0xc0001500 -e main -o build/kernel.bin
+gcc -I lib/kernel/ -I lib/ -Wall -m32 -fno-stack-protector -nostdinc -c -o build/main.o kernel/main.c
+nasm -f elf -o ./build/print.o lib/kernel/print.asm
+ld -m elf_i386 -Ttext 0xc0001500 -e main -o build/kernel.bin build/main.o build/print.o
 dd if=build/kernel.bin of=./hd60M.img bs=512 count=200 seek=9 conv=notrunc
 
--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,11 @@
+#include "print.h"
+
+
 int main(){
+    put_char('6');
+    put_char('6');
+    put_char('6');
+    
     while(1);
     return 0;
 }
