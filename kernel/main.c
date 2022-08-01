95cfd1627f537f12764ac09384233ac416b26555
yaut <3340618946@qq.com>
2022-08-01 19:26:53 +0800

    6.8

--- kernel/main.c
+++ kernel/main.c
@@ -5,6 +5,9 @@ int main(){
     put_char('6');
     put_char('6');
     put_char('6');
+    put_int(0x12133211);
+    put_int(0x00000001);
+    put_str("inasdi");
     
     while(1);
     return 0;
--- lib/kernel/print.asm
+++ lib/kernel/print.asm
@@ -4,6 +4,28 @@ SELECTOR_VIDEO equ (0x0003<<3) + TI_GDT + RPL0
 
 [bits 32]
 section .text
+put_int_buffer dq 0
+global put_str
+put_str:
+    push ebx
+    push ecx
+    xor ecx,ecx
+    mov ebx,[esp+12]
+    .goon:
+    mov cl,[ebx]
+    cmp cl,0
+    jz .str_over
+    push ecx
+    call put_char
+    add esp,4
+    inc ebx
+    jmp .goon
+    .str_over:
+    pop ecx
+    pop ebx
+    ret
+
+
 
 global put_char
 put_char:
@@ -102,5 +124,57 @@ put_char:
     mov al,bl
     out dx,al
 .put_char_done:
+    popad
+    ret
+
+global put_int
+put_int:
+    pushad
+    mov ebp,esp
+    mov eax,[ebp+4*9]
+    mov edx,eax
+    mov edi,7
+    mov ecx,8
+    mov ebx,put_int_buffer
+
+    .16based_4bits:
+    and edx,0x0000000F
+    cmp edx,9
+    jg .is_A2F
+    add edx,'0'
+    jmp .store
+    .is_A2F:
+    sub edx,10
+    add edx,'A'
+    .store:
+    mov [ebx+edi],dl
+    dec edi
+    shr eax,4
+    mov edx,eax
+    loop .16based_4bits
+
+    .ready_to_print:
+    inc edi
+    .skip_prefix_0:
+    cmp edi,8
+    je .full0
+    .go_on_skip:
+    mov cl,[put_int_buffer+edi]
+    inc edi
+    cmp cl,'0'
+    je .skip_prefix_0
+    dec edi
+    jmp .put_each_num
+
+    .full0:
+    mov cl,'0'
+    .put_each_num:
+    push ecx
+    call put_char
+    add esp,4
+    inc edi
+    mov cl,[put_int_buffer+edi]
+    cmp edi,8
+    jl .put_each_num
     popad
     ret
