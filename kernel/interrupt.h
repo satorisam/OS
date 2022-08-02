a8ba4bdc43d7b17efac74b84a6917dc8f0987e38
yaut <3340618946@qq.com>
2022-08-02 17:04:20 +0800

    7.8

---
+++ kernel/interrupt.h
@@ -0,0 +1,8 @@
+#ifndef __KERNEL_INTERRUPT_H
+#define __KERNEL_INTERRUPT_H
+
+typedef intr_handler void*
+
+void idt_init();
+
+#endif
--- kernel/main.c
+++ kernel/main.c
@@ -1,14 +1,13 @@
 #include "print.h"
+#include "init.h"
 
 
 int main(){
-    put_char('6');
-    put_char('6');
-    put_char('6');
-    put_int(0x12133211);
-    put_int(0x00000001);
-    put_str("inasdi");
-    
+    put_str("I am kernel\n");
+    init_all();
+    asm volatile("sti");
+
+
     while(1);
     return 0;
 }
